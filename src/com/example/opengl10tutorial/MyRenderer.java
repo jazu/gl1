2dbe32c445d92cbc7909a2bff814c77640eaadee
unknown <s1200522@WS1200522.bc.local>
2013-12-18 17:59:40 +0200

    finalized collision detection and added gamestates

--- src/com/example/opengl10tutorial/MyRenderer.java
+++ src/com/example/opengl10tutorial/MyRenderer.java
@@ -12,9 +12,9 @@ import android.view.MotionEvent;
 import com.example.opengl10tutorial.R;
 
 public class MyRenderer extends GLSurfaceView implements Renderer {
-
-//TODO:
+	//TODO:
 	// multitouch
+	// add a 3s timer when no touches register between when game has ended and user presses restart so no accidental restarts happen
 	//
 	public MyRenderer(Context context) {
 		super(context);
@@ -47,8 +47,14 @@ public class MyRenderer extends GLSurfaceView implements Renderer {
 	float squarew = 100.0f;
 	float squareh = 100.0f;
 	
-	float maxxv = 20.0f;
-	float maxyv = 20.0f;
+	float maxxv = 25.0f;
+	float maxyv = 25.0f;
+	
+	static final int STATE_STARTED = 1;
+	static final int STATE_GAMEOVER = 2;
+	
+	int gameState = STATE_STARTED;  
+	
 	
 	Square ball = new Square(squarew, squareh);
 	Square popup = new Square(500.0f, 500.0f);
@@ -71,9 +77,7 @@ public class MyRenderer extends GLSurfaceView implements Renderer {
 	    		0.0f, 1.0f,
 	    		1.0f, 1.0f,
 	    		1.0f, 0.0f, 
-
-	    							};
-	//
+	 };
 	
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		gl.glEnable(GL10.GL_BLEND);
@@ -94,62 +98,77 @@ public class MyRenderer extends GLSurfaceView implements Renderer {
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 		gl.glLoadIdentity();
 		
-		gl.glPushMatrix();
-		
-		gl.glTranslatef(ballx, bally, 0.0f);
-		ball.draw(gl);
-		
-		if(bally >= scrh-(squarew/2) || bally <= (squarew/2)) { 
-			ballyv = -ballyv;
-		}
-		if((ballx <= (padw/2)+(squarew/2) && bally <= lpady+(padh/2)+(squareh/2) && bally >= lpady-(padh/2)-(squareh/2)) || //check paddle-ball collision  
-		   (ballx >= scrw-(padw/2)-(squarew/2) && bally <= rpady+(padh/2) && bally >= rpady-(padh/2))) { 
-			ballxv = -ballxv*1.1f; 
-			}
 	
-//		if(ballx <= ){ //check if ball is inside the pad, if so translate ballx EXACTLY outside the padw/2 
-//			
-//		}
-		
-		if (ballx <= 0 || ballx >= scrw) { //popup for losing and resetting 
-			gl.glPopMatrix();
-			gl.glPushMatrix();
-			gl.glTranslatef(scrw/2, scrh/2, 0.0f);
-			popup.draw(gl);
-			gl.glPopMatrix();
+		if(gameState == STATE_STARTED) {
 			
+				gl.glPushMatrix();
+				gl.glTranslatef(ballx, bally, 0.0f);
+				ball.draw(gl);
+				
+				if(bally >= scrh-(squarew/2) || bally <= (squarew/2)) { 
+					ballyv = -ballyv*1.02f;
+				}
+				if((ballx <= (padw/2)+(squarew/2) && bally <= lpady+(padh/2)+(squareh/2) && bally >= lpady-(padh/2)-(squareh/2)) || //check paddle-ball collision  
+				   (ballx >= scrw-(padw/2)-(squarew/2) && bally <= rpady+(padh/2)+(squareh/2) && bally >= rpady-(padh/2)-(squareh/2))) { 
+					ballx = (padw/2)+(squarew/2);
+					ballxv = -ballxv*1.1f; 
+					
+					if(ballxv > 0 )	{
+						ballx = (padw/2)+(squarew/2);
+						}
+						else if(ballxv < 0){ 
+							ballxv = +ballxv*1.1f; 
+							ballx = scrw-(padw/2)-(squarew/2);
+							}
+					}
+				
+				if(ballxv <= -maxxv){  //set max speed for x and y
+					ballxv = -25.0f;
+				}
+				if(ballxv >= maxxv) {
+					ballxv = 25.0f;
+				}
+				if(ballyv <= -maxyv){
+					ballyv = -25.0f;
+				}
+				if(ballyv >= maxyv) {
+					ballyv = 25.0f;
+				}
+				
+				gl.glPopMatrix();
+				
+				gl.glPushMatrix();
+				gl.glTranslatef(scrw-scrw, padstarty, 0.0f);
+				gl.glTranslatef(scrw-scrw, lpady, 0.0f);
+				lpad.draw(gl);
+				gl.glPopMatrix();
+		
+				gl.glPushMatrix();
+				gl.glTranslatef(0, padstarty, 0.0f);
+				gl.glTranslatef(scrw, rpady, 0.0f);
+				rpad.draw(gl);
+				gl.glPopMatrix();
+				
+				ballx+=ballxv;
+				bally+=ballyv;
+				
+		}
+	
+		if (ballx <= 0 || ballx >= scrw) {
+			gameState = STATE_GAMEOVER;
 		}
 		
-		if(ballxv >= maxxv ||  ballyv >= maxyv) {  //set max speed for x and y
-			ballxv = -19.0f;
-			ballyv = -19.0f;
+		if(gameState == STATE_GAMEOVER) {
+		
+				//popup for losing and resetting 
+				gl.glPopMatrix();
+				gl.glPushMatrix();
+				gl.glTranslatef(scrw/2, scrh/2, 0.0f);
+				popup.draw(gl);
+				gl.glPopMatrix();
+			}
 		}
-															//TODO: now if either one (xv or yv) is over the limit both will be reduced to 19, make them separate!
-		if(ballxv <= -maxxv || ballyv <= -maxyv) {
-			ballxv = 19.0f;
-			ballyv = 19.0f;
-		}
-		
-		gl.glPopMatrix();
-		
-		gl.glPushMatrix();
-		gl.glTranslatef(scrw-scrw, padstarty, 0.0f);
-		gl.glTranslatef(scrw-scrw, lpady, 0.0f);
-		lpad.draw(gl);
-		gl.glPopMatrix();
-
-		gl.glPushMatrix();
-		gl.glTranslatef(0, padstarty, 0.0f);
-		gl.glTranslatef(scrw, rpady, 0.0f);
-		rpad.draw(gl);
-		gl.glPopMatrix();
-		
-		ballx+=ballxv;
-//		bally+=ballyv;
-		
-		
-		
-	}
+	
 	
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
 		scrw = width;
@@ -172,24 +191,31 @@ public class MyRenderer extends GLSurfaceView implements Renderer {
 		float x = event.getX();
 		int lside = this.getWidth() / 2;
 		
-		if(event.getAction() == MotionEvent.ACTION_DOWN && ballx <= scrw-scrw-(scrw/2) || ballx >= scrw+(scrw/5)) {  //reset if game has ended and 
-			ballx = ballstartx;																	   //the user touches screen
-			bally = ballstarty;
-			ballxv = -10.0f;
-			ballyv = -10.0f;
+		if(gameState == STATE_GAMEOVER) {
+				if(event.getAction() == MotionEvent.ACTION_DOWN) {  //reset if game has ended and 
+					gameState = STATE_STARTED;						//the user touches screen
+					ballx = ballstartx;																	  			 
+					bally = ballstarty;
+					ballxv = -5.0f;
+					ballyv = -5.0f;
+					
+					
+				}
 		}
 		
-		if(event.getAction() == MotionEvent.ACTION_MOVE) {
-			
-			
-		            	if(x < lside) {
-		    				lpady = y;
-		    			}
-			
-		    			if(x > lside) {
-		    				rpady = y;
-		    			}
-		        }
+		if(gameState == STATE_STARTED) {
+				if(event.getAction() == MotionEvent.ACTION_MOVE || ballx <= 0-(scrw/2) || ballx >= scrw+(scrw/2)) {
+				
+				            	if(x < lside) {
+				    				lpady = y;
+				    			}
+					
+				    			if(x > lside) {
+				    				rpady = y;
+				    			}
+				        }
+		}
 		return true;
 		}
+	
 	}
